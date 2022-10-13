8b72db91ea139aca16fcf8c3a1e761d8f7b221ee
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-10-13 17:38:32 -0400

    done

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from statistics import mode
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import confusion_matrix
 from ast import arg
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, Dropout
 import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
@@ -13,7 +16,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # PLS DO NOT EXCEED THIS TIME LIMIT
-MAXIMIZED_RUNNINGTIME=1000
+MAXIMIZED_RUNNINGTIME = 1000
 # REPRODUCE THE EXP
 seed = 123
 random.seed(seed)
@@ -26,17 +29,16 @@ parser = ArgumentParser()
 # Hint: Advanced optimizer will achieve better performance.
 # Hint: Large Epochs will achieve better performance.
 # Hint: Large Hidden Size will achieve better performance.
-parser.add_argument("--optimizer", default='sgd', type=str)
-parser.add_argument("--epochs", default=100, type=int)
+parser.add_argument("--optimizer", default='RMSprop', type=str)
+parser.add_argument("--epochs", default=50, type=int)
 parser.add_argument("--hidden_size", default=255, type=int)
 parser.add_argument("--scale_factor", default=255, type=float)
-parser.add_argument("--batch_size", default=32, type=int)
+parser.add_argument("--batch_size", default=64, type=int)
 ###########################MAGIC ENDS HERE##########################
 
 parser.add_argument("--is_pic_vis", action="store_true")
 parser.add_argument("--model_output_path", type=str, default="./output")
 parser.add_argument("--model_nick_name", type=str, default=None)
-
 
 
 args = parser.parse_args()
@@ -49,7 +51,8 @@ if os.path.exists(args.model_output_path) is False:
     os.mkdir(args.model_output_path)
 
 if args.model_nick_name is None:
-    setattr(args, "model_nick_name", f"OPT_{args.optimizer}-E_{args.epochs}-H_{args.hidden_size}-S_{args.scale_factor}")
+    setattr(args, "model_nick_name",
+            f"OPT_{args.optimizer}-E_{args.epochs}-H_{args.hidden_size}-S_{args.scale_factor}")
 
 '''
 1. Load the dataset
@@ -58,14 +61,14 @@ Please do not change this code block
 class_names = {
     0: "airplane",
     1: "automobile",
-    2:"bird",
-    3:"cat",
-    4:"deer",
-    5:"dog",
-    6:"frog",
-    7:"horse",
-    8:"ship",
-    9:"truck"
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck"
 }
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
@@ -79,14 +82,15 @@ x_test = x_test[:, :, :, 0]
 
 # split the training dataset into training and validation
 # 70% training dataset and 30% validation dataset
-x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.3, random_state=seed, stratify=y_train)
+x_train, x_valid, y_train, y_valid = train_test_split(
+    x_train, y_train, test_size=0.3, random_state=seed, stratify=y_train)
 
 
 if args.is_pic_vis:
     # Visualize the image
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     for i in range(25):
-        plt.subplot(5,5,i+1)
+        plt.subplot(5, 5, i+1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
@@ -100,7 +104,7 @@ if args.is_pic_vis:
 '''
 # Scale the image
 ###########################MAGIC HAPPENS HERE##########################
-# Scale the data attributes 
+# Scale the data attributes
 # Hint: Scaling the data in the range 0-1 would achieve better results.
 x_train = x_train / args.scale_factor
 x_valid = x_valid / args.scale_factor
@@ -125,19 +129,25 @@ model = Sequential()
 ###########################MAGIC HAPPENS HERE##########################
 # Build up a neural network to achieve better performance.
 # Hint: Deeper networks (i.e., more hidden layers) and a different activation function may achieve better results.
-model.add(Flatten())
-model.add(Dense(args.hidden_size, activation="relu")) # first layer
-model.add(Dense(args.hidden_size, activation="relu")) # second layer
-model.add(Dense(args.hidden_size, activation="relu")) # second layer
-
+# model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 1)))
+# model.add(MaxPooling2D(pool_size=2))
+# model.add(Flatten())
+model.add(Dense(512, activation='relu', input_shape=(32, 32)))
+model.add(Dense(256, activation='selu'))
+model.add(Dense(128, activation="tanh"))  # first dense layer
+model.add(Dense(64, activation="relu")) 
+model.add(Dense(32, activation='relu')) # second dense layer with less nodes
+model.add(Dense(num_labels, activation='relu')) # second dense layer with less nodes
+model.add(Flatten())    
 
 ###########################MAGIC ENDS HERE##########################
-model.add(Dense(num_labels)) # last layer
+model.add(Dense(num_labels))  # last layer
 
 # Compile Model
 model.compile(optimizer=args.optimizer,
 
-              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=keras.losses.SparseCategoricalCrossentropy(
+                  from_logits=True),
               metrics=['accuracy'])
 
 # Train Model
@@ -160,9 +170,6 @@ model.save(args.model_output_path + "/" + args.model_nick_name)
 '''
 
 y_test_predict = np.argmax(model.predict(x_test), axis=1)
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_score, recall_score
-import matplotlib.pyplot as plt
 
 ###########################MAGIC HAPPENS HERE##########################
 # Visualize the confusion matrix by matplotlib and sklearn based on y_test_predict and y_test
@@ -195,4 +202,20 @@ print("Recall: ", recall_score(y_test, y_test_predict, average=None))
 # plot the recall
 plt.plot(recall_score(y_test, y_test_predict, average=None))
 plt.title("Recall")
+plt.show()
+
+# plot epoch vs accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+#  print the image of the misclassified images
+misclassified_idx = np.where(y_test_predict != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(x_test[i])
+plt.title("True label: %s Predicted: %s" % (class_names[y_test[i][0]], class_names[y_test_predict[i]]));
 plt.show()
